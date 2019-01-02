d6e24e78c563b3120271b3dd33576249059c1aa7
William <1182693928@qq.com>
2019-01-02 11:21:09 +0800

    Initial Commit

---
+++ ScanIDCardDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScanIDCardDemo
+//
+//  Created by William on 2019/1/2.
+//  Copyright © 2019 William. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
